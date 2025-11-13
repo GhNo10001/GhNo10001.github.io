1f761f11a42cecf254d12b66857da4ce4ad2fa67
GhNo10001 <x18825407105@outlook.com>
2025-11-13 15:22:07 +0800

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,8 +1,37 @@
 import { Config } from './config.js';
+
 export const State = {
-  ...Config.time.start,
-  hp:100,maxHp:100,mp:50,maxMp:50,exp:0,lv:0,atk:10,def:5,wux:10,qiy:50,
-  root:'五行杂灵根',sect:'散修',bag:[],cd:0,
-  entropy:{spirit:0,morality:0,order:0,life:0},
-  flags:{}
+    // 时间状态
+    ...Config.time.start,
+    
+    // 角色属性
+    hp: 100,
+    maxHp: 100,
+    mp: 50,
+    maxMp: 50,
+    exp: 0,
+    lv: 0,
+    atk: 10,
+    def: 5,
+    wux: 10,
+    qiy: 50,
+    root: '五行杂灵根',
+    sect: '散修',
+    reputation: 0,
+    
+    // 物品与冷却
+    bag: [],
+    cd: 0,
+    
+    // 熵变状态
+    entropy: {
+        spirit: 0,
+        morality: 0,
+        order: 0,
+        life: 0
+    },
+    
+    // 剧情标记
+    flags: {},
+    tags: new Set()
 };
